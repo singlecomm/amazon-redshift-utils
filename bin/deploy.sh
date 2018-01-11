367bff00b38341b6ebb4b3b91ef5dadd732b291d
Eric Meisel <eric.steven.meisel@gmail.com>
2018-01-11 10:25:08 -0600

    chore(PET-270): Cleaning up kubernetes config

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,10 @@ else
     BRANCH="${CONTEXT}"
 fi
 
+if [ -d "kubernetes-config-${CONTEXT}" ]; then
+  rm -rf "kubernetes-config-${CONTEXT}"
+fi
+
 git clone git@github.com:singlecomm/kubernetes-config.git -b "${BRANCH}" "kubernetes-config-${CONTEXT}"
 sed -i "s/singlecomm\\/amazon-redshift-utils:v[0-9]\\+.[0-9]\\+.[0-9]\\+/singlecomm\\/amazon-redshift-utils:$TAG/" kubernetes-config-${CONTEXT}/${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
 kubectl --context="${CONTEXT}" --namespace="${CONTEXT}-coral" apply -f kubernetes-config-${CONTEXT}/${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
@@ -16,3 +20,5 @@ cd kubernetes-config-${CONTEXT}
 git add ${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
 git commit -m "chore(release): Amazon Redshift Utils Version $TAG"
 git push origin ${BRANCH}
+cd ..
+rm -rf "kubernetes-config-${CONTEXT}"
