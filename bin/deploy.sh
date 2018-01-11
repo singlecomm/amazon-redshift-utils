f6da4bd4f9bffa71cc4c9e958433c38b8f38c491
Eric Meisel <eric.steven.meisel@gmail.com>
2018-01-11 10:04:42 -0600

    chore(PET-270) Fixing prod branch reference

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-BRANCH=${1:-}
+CONTEXT=${1:-}
 TAG=${2:-${TAG}}
 
-git clone git@github.com:singlecomm/kubernetes-config.git -b "${BRANCH}" "kubernetes-config-${BRANCH}"
-sed -i "s/singlecomm\\/amazon-redshift-utils:v[0-9]\\+.[0-9]\\+.[0-9]\\+/singlecomm\\/amazon-redshift-utils:$TAG/" kubernetes-config-${BRANCH}/${BRANCH}-coral/cronjobs/analyze-vacuum-redshift.yml
-kubectl --context="${BRANCH}" --namespace="${BRANCH}-coral" apply -f kubernetes-config-${BRANCH}/${BRANCH}-coral/cronjobs/analyze-vacuum-redshift.yml
-cd kubernetes-config-${BRANCH}
-git add ${BRANCH}-coral/cronjobs/analyze-vacuum-redshift.yml
+if [[ ${CONTEXT} == 'prod' ]]; then
+    BRANCH='master'
+else
+    BRANCH="${CONTEXT}"
+fi
+
+git clone git@github.com:singlecomm/kubernetes-config.git -b "${BRANCH}" "kubernetes-config-${CONTEXT}"
+sed -i "s/singlecomm\\/amazon-redshift-utils:v[0-9]\\+.[0-9]\\+.[0-9]\\+/singlecomm\\/amazon-redshift-utils:$TAG/" kubernetes-config-${CONTEXT}/${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
+kubectl --context="${CONTEXT}" --namespace="${CONTEXT}-coral" apply -f kubernetes-config-${CONTEXT}/${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
+cd kubernetes-config-${CONTEXT}
+git add ${CONTEXT}-coral/cronjobs/analyze-vacuum-redshift.yml
 git commit -m "chore(release): Amazon Redshift Utils Version $TAG"
 git push origin ${BRANCH}
